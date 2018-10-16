303248a181193a793bc7bd4a2dd7653c1f887b2f
gopal  goyal <44091979+gopalgoyal1999@users.noreply.github.com>
2018-10-16 23:18:44 +0530

    code

---
+++ precache-manifest.290ae70729ada35a6d1d1df9bef48188.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a0c94e8aafcf78a4774",
+    "url": "/onlineshopapp1/static/js/runtime~main.3a0c94e8.js"
+  },
+  {
+    "revision": "a966d516da6b650d0932",
+    "url": "/onlineshopapp1/static/js/main.a966d516.chunk.js"
+  },
+  {
+    "revision": "87aedec117454d9f8af4",
+    "url": "/onlineshopapp1/static/js/1.87aedec1.chunk.js"
+  },
+  {
+    "revision": "a966d516da6b650d0932",
+    "url": "/onlineshopapp1/static/css/main.908c982d.chunk.css"
+  },
+  {
+    "revision": "87aedec117454d9f8af4",
+    "url": "/onlineshopapp1/static/css/1.502cd677.chunk.css"
+  },
+  {
+    "revision": "e7bd3be2a22d4e9b53217fccdf9659a7",
+    "url": "/onlineshopapp1/index.html"
+  }
+];
